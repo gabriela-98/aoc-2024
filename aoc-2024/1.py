57a408aea01155976cc696613cb23136841150b7
Gabriela Vidovic <gabriela.vidovic@wearenotch.com>
2024-12-02 18:05:20 +0100

    First aoc task

---
+++ aoc-2024/1.py
@@ -0,0 +1,16 @@
+import numpy as np
+from data import data
+
+data = np.array(data.split())
+arr_left = data[::2]
+arr_right = data[1::2]
+sortedL = np.array(sorted(arr_left)).astype(int)
+sortedR = np.array(sorted(arr_right)).astype(int)
+
+result = [abs(sortedL[i] - sortedR[i]) for i in range(len(sortedL))]
+result2 = [i * np.count_nonzero(sortedR == i) for i in sortedL if np.count_nonzero(sortedR == i) > 0]
+
+print("A: ", sum(result))
+print("B: ", sum(result2))
+
+
